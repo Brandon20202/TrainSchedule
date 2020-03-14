08d608a223b8143e82c65813afa8941216e360e5
Brandon20202 <brandon.taylor@austinisd.org>
2020-03-14 15:54:07 -0500

    changed html tables to Jquery adding tables, fixed firebase urls

--- assets/app.js
+++ assets/app.js
@@ -1,63 +1,96 @@
 var counter =0;
 
-var Config = {
+var config = {
     apiKey: "AIzaSyDWlFxDEJNyiPbZk43lJNUTspUj4jKa9ds",
     authDomain: "trainschedule-9cf77.firebaseapp.com",
     databaseURL: "https://trainschedule-9cf77.firebaseio.com/",
     projectId: "trainschedule-9cf77",
     storageBucket: "trainschedule-9cf77.appspot.com",
-    messagingSenderId: "209290900216",
-    appId: "1:209290900216:web:92227264757055c32d010d"
+   
 };
   // Initialize Firebase
-  firebase.initializeApp(Config);
+  firebase.initializeApp(config);
+  var data = firebase.database();
+ 
 
-  var database = firebase.databaseURL();
+  
 
   $("#submit-button").on("click", function(){
       event.preventDefault();
 
-      var trainName=$("#name-input").val().trim();
+       var trainName=$("#name-input").val().trim();
       $("#name-input").val("");
       
       var trainDestination=$("#Destination-input").val().trim();
       $("#Destination-input").val("");
       
       var trainTime=$("#First-Train-Time-input").val().trim();
-      $("#Frist-Train-Time-input").val("");
+      $("#First-Train-Time-input").val("");
       
       var trainFrequency=$("#Frequency-input").val().trim();
       $("#Frequency-input").val("");
       
-      database.ref().push({
+      data.ref().push({
           tName: trainName,
           destination: trainDestination,
           time: trainTime,
           frequency: trainFrequency
 
         });
-        $("#first-shell-table").text(trainName);
-        $("second-shell-table").text(trainDestination);
+    
 
 
   });
 
 
-database.ref().on('child_added', function(childSnapshot) {
+data.ref().on('child_added', function(childSnapshot) {
     console.log(childSnapshot.val().tName);
     console.log(childSnapshot.val().destination);
     console.log(childSnapshot.val().time);
     console.log(childSnapshot.val().frequency);
 
+    var newRow = $("<tr>");
+	newRow.attr("id", "train-row-" + counter);
+	$("#train-table").append(newRow);
+
+	var trainNameTd = $("<td>", {id: "train-name-td"});
+	trainNameTd.html(childSnapshot.val().tName);
+	$("#train-row-" + counter).append(trainNameTd);
+
+	
+	var trainDestinationTd = $("<td>", {id: "train-destination-td"});
+	trainDestinationTd.html(childSnapshot.val().destination);
+    $("#train-row-" + counter).append(trainDestinationTd);
+    
+
+
+    
     var firstTimeConverted = moment(childSnapshot.val().time, "hh:mm").subtract(1, "years");
-    var currentTime = moment();
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     var tRemainder = diffTime % childSnapshot.val().frequency;
     var tMinutesTillTrain = childSnapshot.val().frequency - tRemainder; 
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     var nextTrainFormat = moment(nextTrain).format("hh:mm");
 
-    counter++;
+
+    
+	var trainFrequencyTd = $("<td>", {id: "train-frequency-td"});
+	trainFrequencyTd.html(childSnapshot.val().frequency);
+	$("#train-row-" + counter).append(trainFrequencyTd);
+
+	
+	var nextTrainTd = $("<td>", {id: "next-train-td"});
+	nextTrainTd.text(nextTrainFormat);
+	$("#train-row-" + counter).append(nextTrainTd);
+
+	var minutesAwayTd = $("<td>", {id: "minutes-away-td"});
+	minutesAwayTd.text(tMinutesTillTrain);
+	$("#train-row-" + counter).append(minutesAwayTd);
+
+	counter++;
+
+
+ 
 
 }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
